ad17317d5c00c208d62fe0d9baae652d7f447c3b
Alexander Vega <87383623+AlexVega2001@users.noreply.github.com>
2023-03-30 12:15:07 -0500

    Se agregaron paginas, modulos, componentes y más

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
+// Módulos
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MoviesModule } from './movies/movies.module';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
+// Componentes
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MoviesModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
